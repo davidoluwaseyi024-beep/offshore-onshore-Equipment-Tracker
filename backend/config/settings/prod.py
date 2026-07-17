708deb11e06103dd0566611d8f224c0d7e0b4aa4
sultan <martinsnaskid@gmail.com>
2026-07-18 00:10:36 +0100

    trigger ci cd test

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -57,3 +57,4 @@ STORAGES = {
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")  # noqa: F405
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# ci/cd test trigger
